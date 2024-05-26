c139210e183425ae01237006731832d5458b6bc1
Rdosrun <rdosrun@gmail.com>
2024-05-25 21:27:57 -0600

    attempting to add ffmpeg option to database

--- arm/migrations/versions/3e2903b18eb2_ffmpeg_support.py
+++ arm/migrations/versions/3e2903b18eb2_ffmpeg_support.py
@@ -17,12 +17,18 @@ depends_on = None
 
 
 def upgrade():
-    sa.Column('FFMPEG_CLI', sa.String(length=256), nullable=True),
-    sa.Column('FFMPEG_LOCAL', sa.String(length=256), nullable=True),
-    sa.Column('USE_FFMPEG', sa.Boolean(), nullable=True),
-    sa.Column('FFMPEG_ARGS', sa.String(length=512), nullable=True),
+    op.add_column('config',
+                sa.Column('FFMPEG_CLI', sa.String(length=256), nullable=True),
+                sa.Column('FFMPEG_LOCAL', sa.String(length=256), nullable=True),
+                sa.Column('USE_FFMPEG', sa.Boolean(), nullable=True),
+                sa.Column('FFMPEG_ARGS', sa.String(length=512), nullable=True),
+                  )
     pass
 
 
 def downgrade():
+    op.drop_column('config', 'FFMPEG_CLI')
+    op.drop_column('config', 'FFMPEG_LOCAL')
+    op.drop_column('config', 'USE_FFMPEG')
+    op.drop_column('config', 'FFMPEG_ARGS')
     pass
